23b42989b65ee338cf0376ce8cd6c56b02dece96
Lance Mungcal <mlancegerald@.com>
2025-10-01 21:29:16 +0800

    installation in terminal success?

---
+++ src/server/db/schema.ts
@@ -0,0 +1,24 @@
+import { sql } from "drizzle-orm";
+import { pgTableCreator } from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+
+
+export const createTable = pgTableCreator((name) => `natsirt${name}`);
+
+export const apiKeys = createTable("api_keys", (d) => ({
+  id: d.text("id").primaryKey(),
+  name: d.varchar("name", { length: 256 }).notNull(),
+  hashedKey: d.text("hashed_key").notNull(),
+  last4: d.varchar("last4", { length: 4 }).notNull(),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  revoked: d.boolean("revoked").notNull().default(false),
+}));
