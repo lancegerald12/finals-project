411fc0dfd2935c64a689b906edb82bbeb91f0345
Lance Mungcal <mlancegerald@.com>
2025-10-05 22:25:19 +0800

    creating database

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,7 +8,6 @@ import { pgTableCreator } from "drizzle-orm/pg-core";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 
-
 export const createTable = pgTableCreator((name) => `natsirt${name}`);
 
 export const apiKeys = createTable("api_keys", (d) => ({
